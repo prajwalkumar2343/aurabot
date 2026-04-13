425061604b2b9cf2074a6d9141349593adff86ef
Prajwal Kumar <145465184+prajwalkumar2343@users.noreply.github.com>
2026-04-14 02:30:32 +0530

    chore: update Package.swift dependencies

--- aurabot-swift/Package.swift
+++ aurabot-swift/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "AuraBot",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     products: [
         .executable(
             name: "AuraBot",
